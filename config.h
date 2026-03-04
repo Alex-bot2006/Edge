054137e40c7245f690df65a17370cd9cb0872794
Alex-bot2006 <59910935+Alex-bot2006@users.noreply.github.com>
2026-03-04 17:34:48 +0200

    Delete config.h

--- config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Your secret OpenAI API Key
-//const String OPENAI_API_KEY = "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";
